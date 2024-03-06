78be1e4cdf81e925b357c536496c302f81e06e12
Scylin232 <timyr3@ukr.net>
2024-03-06 10:53:38 +0100

    Initial Commit;

---
+++ ScyScaffPlugin.SvelteCrud/SvelteCrud.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using ScyScaff.Core.Models.Plugins;
+
+namespace ScyScaffPlugin.SvelteCrud;
+
+public class SvelteCrud : IDashboardTemplatePlugin
+{
+    public string DashboardName => "svelte-crud";
+    
+    public string GetTemplateTreePath() => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
+}
