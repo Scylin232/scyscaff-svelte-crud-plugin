ab7fe481bf4e983ce9d8f4de8b1bd1b6b79a8085
Scylin232 <timyr3@ukr.net>
2024-04-16 04:51:17 +0200

    Dockerized;

--- ScyScaffPlugin.SvelteCrud/SvelteCrud.cs
+++ ScyScaffPlugin.SvelteCrud/SvelteCrud.cs
@@ -1,11 +1,38 @@
 ﻿using System.Reflection;
+using ScyScaff.Core.Models.Parser;
 using ScyScaff.Core.Models.Plugins;
+using ScyScaff.Docker.Enums;
+using ScyScaff.Docker.Models.Builder;
+using ScyScaff.Docker.Models.Plugins;
 
 namespace ScyScaffPlugin.SvelteCrud;
 
-public class SvelteCrud : IDashboardTemplatePlugin
+public class SvelteCrud : IDashboardTemplatePlugin, IDockerCompatible
 {
     public string DashboardName => "svelte-crud";
     
     public string GetTemplateTreePath() => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
+    
+    public IEnumerable<DockerComposeService> GetComposeServices(string projectName, ScaffolderService? service, string serviceName, int serviceIndex)
+    {
+        List<DockerComposeService> dockerComposeServices = new()
+        {
+            new DockerComposeService
+            {
+                Type = DockerComposeServiceType.Dashboard,
+                ContainerName = $"{projectName.ToLower()}-dashboard",
+                Build = new ComposeBuild
+                {
+                    Context = $"./{projectName}.Dashboard/",
+                    Dockerfile = "Dockerfile.dev"
+                },
+                Ports = new Dictionary<int, int?>
+                {
+                    { 5353, 5353 }
+                }
+            }
+        };
+
+        return dockerComposeServices;
+    }
 }
